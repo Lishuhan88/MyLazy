aa92cffaf1c7f9503fbb4f75a1a187c35996ae23
Lishuhan88 <86051938+Lishuhan88@users.noreply.github.com>
2024-08-05 09:38:55 +0800

    Update treesitter.lua
    把treesitter的格式化代码关了，只提供高亮功能

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,11 +5,13 @@ return{
         build = ":TSUpdate",
         opts = {
             ensure_installed = {"c", "cpp", "cmake",},
+            -- 更好的高亮代码，包开的啊
             highlight = {
                 enable = true,
             },
+            -- 更好的格式化代码，有点问题先关了
             indent = {
-                enable = true,
+                enable = false,
             },
         },
     },
