573b8c43459e20dc5b7a83ddc17aac79464f88db
Lishuhan88 <86051938+Lishuhan88@users.noreply.github.com>
2024-08-05 09:14:15 +0800

    Update options.lua
    规整了option配置

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,39 +1,49 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local option = vim.opt
+local buffer = vim.b
+local global = vim.g
 
-local opt = vim.opt
-
--- 缩进
-opt.exrc = true --允许每个目录下都有.nvim.lua文件
-opt.relativenumber = false --关闭相对行号
-opt.autoindent = true --设置自动缩进
-opt.cindent = true --启用c/c++风格缩进
-
-opt.smartindent = true --智能的选择对其方式
-
-opt.expandtab = true --将制表符扩展为空格
-opt.tabstop = 4 --设置编辑时制表符占用空格数
-opt.shiftwidth = 4 --设置格式化时制表符占用空格数
-opt.softtabstop = 4 --设置4个空格为制表符
-opt.smarttab = true --在行和段开始处使用制表符
-vim.cmd("set backspace=2") --使用回车键正确处理indent,eol,start等
-opt.sidescroll = 10 --设置向右滚动字符数
-vim.cmd("set tw=0")
-vim.cmd("set indentexpr=")
-vim.cmd("set autochdir")
-
+-- Globol Settings --
+option.showmode = false --不显示左下角的insert等状态
+option.backspace = { "indent", "eol", "start" }
+option.tabstop = 4
+option.shiftwidth = 4
+option.expandtab = true
+option.shiftround = true
+option.autoindent = true
+option.smartindent = true
+option.number = true
+option.relativenumber = false
+option.wildmenu = true --命令行可以自动补全
+option.ignorecase = true --模糊搜索
+option.smartcase = true
+option.completeopt = { "menuone", "noselect" }
+option.cursorline = true --高亮显示当前行
+option.termguicolors = true --真彩开启
+option.signcolumn = "yes" --给左边栏留更多位置
+option.autoread = true
+option.title = true
+option.swapfile = false --和强制退出相关
+option.backup = false
+option.updatetime = 50 --触发响应时间
+option.mouse = "a" --所有模式启用鼠标
+option.exrc = true --支持每个工作目录下有一个局部的.nvim.lua文件
+option.wrap = true --当一行满的时候会自动显示到下一行
+option.splitright = true --打开分屏默认在右边显示
+option.scrolloff = 5 --光标移动到buffer的顶部和底部时保持五行距离
 vim.cmd("autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o") --解决o携带注释插入下一行的问题
 
--- 超出屏幕范围的内容自动换行显示
-opt.wrap = true
+-- Buffer Settings --
+buffer.fileenconding = "utf-8"
+
+-- Global Settings --
+global.mapleader = " " --设置空格为leader键
+
 
 -- 禁止保存文件时自动格式化
 vim.g.autoformat = false
-
---编码
---opt.encoding = "utf-8"
---opt.fileencoding = "utf-8"
 
 --根据项目不同在项目根目录下创建.nvim.lua文件
 -- require("lspconfig").clangd.setup({
