ba40eace041d990f5b57814607c769f5f505a7d5
Lishuhan88 <86051938+Lishuhan88@users.noreply.github.com>
2025-02-17 10:14:46 +0800

    Update cmp.lua
    之前提交的那个禁用自动导入，直接把自动补全给禁用了。。。这次删除掉那个设置

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -37,10 +37,6 @@ return {
                 { name = 'luasnip' },
                 { name = "buffer" },
             },
-            -- 禁用自动导入
-            completion = {
-                autocomplete = false,
-            },
             mapping = cmp.mapping.preset.insert {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
